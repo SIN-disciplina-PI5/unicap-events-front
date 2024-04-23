f1c1b827b439552568b55cccad6e6ba891d63ae6
Larissa Albuquerque <larissaamca@gmail.com>
2024-04-23 11:32:41 -0300

    feat new bar progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
+import NProgress from 'nprogress';
+import '../styles/Nprogress.css';
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from '@chakra-ui/react'
@@ -11,6 +13,11 @@ import theme from "../styles/themes";
 export default function App({ Component, pageProps }: AppProps) {
   const [showLayout, setShowLayout] = useState<boolean>(false);
   const { asPath } = useRouter();
+
+  //Route Events. 
+  Router.events.on('routeChangeStart', () => NProgress.start());
+  Router.events.on('routeChangeComplete', () => NProgress.done());
+  Router.events.on('routeChangeError', () => NProgress.done());
 
   useEffect(() => {
     if (layoutRoutes.includes(asPath)) setShowLayout(true);
